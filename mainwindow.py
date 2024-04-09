d1122065aa521c7254bb13b77e860d288ed33723
lowskii <lowskillboy03@gmail.com>
2024-04-09 16:12:03 +0300

    Добавлен футтер

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from creational.singleton import Singleton
 import eqswindow
+import footerwindow
 
 class Window(Tk, Singleton):
     def init(self):
@@ -10,9 +11,16 @@ class Window(Tk, Singleton):
         self.button = Button(self, text='Open EQs', command=self.create_eqs_window)
         self.button.pack(expand=True)
 
+        self.button = Button(self, text='Show footer', command=self.create_eqs_footer)
+        self.button.pack(expand=True)
+
     def create_eqs_window(self):
         global extraWindow
         extraWindow = eqswindow.Extra()
+
+    def create_eqs_footer(self):
+        global extraWindow
+        extraWindow = footerwindow.Extra()
     
     def __init__(self):
         print('calling  from __init__')
